dfdea11991f77d09f5b6d8c6570e44bc0fcd82c0
Martin Ruiz <martin.ru@outlook.com>
2026-03-29 21:04:03 -0600

    replace Footer component with AppFooter component

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+footer
+  UContainer
+    div(class="flex flex-col sm:flex-row items-center justify-between gap-4 py-6")
+      div(class="text-sm text-muted") &copy; {{ new Date().getFullYear() }} ElCrew. All rights reserved.
+</template>
+
+<style scoped></style>
